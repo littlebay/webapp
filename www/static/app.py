b3d6025ef68eb2cdbe51848a9d617e3b0cbfd660
unknown <549436901@qq.com>
2015-09-30 23:03:31 +0800

    some test and def select

--- www/static/app.py
+++ www/static/app.py
@@ -36,3 +36,18 @@ def create_pool(loop,**kw):
 		minsize=kw.get('minsize',1),
 		loop=loop
 	)
+	
+@asyncio.coroutine
+def select(aql,args,size=None):
+	log(sql,args)
+	global __pool
+	with (yield from __pool) as conn:
+		cur=yield from conn.cursor(aiomysql.DictCursor)
+		yield from cur.execute(sql.repalace('?','%s'),args or())
+		if size:
+			rs=yield from cur.fetchmany(size)
+		else:
+			rs=yield from cur.fetchall()
+		yield from cur.close()
+		logging.info('rows returned:%s'%len(rs))
+		return rs
