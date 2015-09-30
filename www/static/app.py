01eceafa2b86cd6f64ab23f9b99a0ff766e89650
unknown <549436901@qq.com>
2015-09-30 21:59:04 +0800

    create databae connectin pool...

--- www/static/app.py
+++ www/static/app.py
@@ -19,3 +19,20 @@ def init(loop):
 loop=asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
+
+@asyncio.croutine
+def create_pool(loop,**kw):
+	logging.info('create databass connection pool...')
+	global __pool
+	__pool=yield from aiomysql.creat_pool(
+		host=kw.get('host','localhost'),
+		port=kw.get('port',3306),
+		user=kw['user'],
+		password=kw['password'],
+		db=kw['db'],
+		charset=kw.get('charset','urf-8'),
+		autocommit=kw.get('autocommit',Ture),
+		maxsize=kw.get('maxsize',10),
+		minsize=kw.get('minsize',1),
+		loop=loop
+	)
